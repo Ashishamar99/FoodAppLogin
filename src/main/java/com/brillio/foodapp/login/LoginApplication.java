06aba87e6dac40cc1e0ff986eb810d1ed6b680ad
Ashish Amar <ashishamar1999@gmail.com>
2021-09-16 12:06:56 +0530

    Basic Login Application Done.

--- src/main/java/com/brillio/foodapp/login/LoginApplication.java
+++ src/main/java/com/brillio/foodapp/login/LoginApplication.java
@@ -2,12 +2,16 @@ package com.brillio.foodapp.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class LoginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginApplication.class, args);
+		BCryptPasswordEncoder bCrypt = new BCryptPasswordEncoder();
+		System.out.println(bCrypt.encode("1234"));
+		System.out.println(bCrypt.encode("admin"));
 	}
 
 }
